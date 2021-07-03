cb012af8d5fbd0ea4d1798881771449ff470c9bf
Tim Stadler <timstadler905@googlemail.com>
2021-07-03 22:46:38 +0200

    Add Friction

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -3,20 +3,41 @@ using UnityEngine;
 public class BallScript : MonoBehaviour
 {
   Rigidbody2D _rigidBody;
+  public float DefaultFriction = 0.99f;
+  public float CurrentFriction;
+
   // Start is called before the first frame update
   void Start()
   {
     _rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    CurrentFriction = DefaultFriction;
   }
 
   // Update is called once per frame
   void Update()
   {
-
+    if(_rigidBody.velocity.magnitude > 0)
+    {
+      _rigidBody.velocity *= CurrentFriction;
+    }
   }
 
   public void Shoot(Vector2 direction)
   {
     _rigidBody.AddForce(direction);
+  }
+
+  private void OnTriggerStay2D(Collider2D collision)
+  {
+    var friction = collision.gameObject.GetComponent<Friction>();
+    if (friction != null)
+      CurrentFriction = friction.FrictionValue;
+  }
+
+  private void OnTriggerExit2D(Collider2D collision)
+  {
+    var friction = collision.gameObject.GetComponent<Friction>();
+    if (friction != null)
+      CurrentFriction = DefaultFriction;
   }
 }
